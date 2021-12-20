2366dea5abc06537c6a184d1fd9edc2b42743f3f
Arnas Babeckis <ababeckis@dcscorp.com>
2021-12-20 11:57:44 -0500

    Added a sample contract for listing ares devices as well as a project containing the implementation of said contract.

---
+++ AresCoreServices/DevicesService.cs
@@ -0,0 +1,32 @@
+﻿using Ares.Core.Messages.Device;
+using AresLib;
+using Grpc.Core;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AresCoreServices
+{
+  public class DevicesService : AresDevices.AresDevicesBase
+  {
+    private readonly ILaboratoryManager _laboratoryManager;
+    public DevicesService(ILaboratoryManager laboratoryManager)
+    {
+      _laboratoryManager = laboratoryManager;
+    }
+
+    public override Task<ListAresDevicesResponse> ListAresDevices(ListAresDevicesRequest _, ServerCallContext context)
+    {
+      var aresDeviceMessages = _laboratoryManager.Lab.DeviceInterpreters
+        .Select(interpreter => interpreter.Device)
+        .Select(device => device.Name)
+        .Select(s => new AresDevice { Name = s });
+
+      var response = new ListAresDevicesResponse
+      {
+        AresDevices = { aresDeviceMessages }
+      };
+
+      return Task.FromResult(response);
+    }
+  }
+}
---
+++ AresCoreServices/ServiceMapper.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace AresCoreServices
+{
+  public static class ServiceMapper
+  {
+    public static void MapCoreAresServices(this IEndpointRouteBuilder routeBuilder)
+    {
+      routeBuilder.MapGrpcService<DevicesService>();
+    }
+  }
+}
