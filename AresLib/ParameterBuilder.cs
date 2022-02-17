d1db819cca5abde170d15872df1ecb9b08499d22
Andrew Armstrong <aarmstrong@infoscitex.com>
2022-02-17 10:12:33 -0500

    Throw exception if parameters build and invalid result. Started considering completed campaigns

--- AresLib/ParameterBuilder.cs
+++ AresLib/ParameterBuilder.cs
@@ -1,4 +1,6 @@
-﻿using Ares.Messaging;
+﻿using System;
+using System.Linq;
+using Ares.Messaging;
 
 namespace Ares.AutomationBuilding;
 
@@ -18,7 +20,33 @@ public class ParameterBuilder : IParameterBuilder
         Value = (float)Value
       };
 
+    ThrowIfInvalid(commandParameter);
+
     return commandParameter;
+  }
+
+  private void ThrowIfInvalid(Parameter parameter)
+  {
+    if (parameter.Planned)
+    {
+      return;
+    }
+
+    if (!parameter.Metadata.Constraints.Any())
+    {
+      return;
+    }
+
+    // TODO: There could be more than 1 "limits" (imagine discrete ranges of limits) that are not handled yet.
+    var limits = parameter.Metadata.Constraints[0];
+
+    if (parameter.Value >= limits.Minimum
+        && parameter.Value <= limits.Maximum)
+    {
+      return;
+    }
+
+    throw new Exception($"{parameter.Metadata.Name} with value {parameter.Value} is invalid with constraints {parameter.Metadata.Constraints.Select(l => $"[{l.Minimum}, {l.Maximum}] ").Aggregate((left, right) => $"{left}, {right}")}");
   }
 
   public ParameterMetadata Metadata { get; }
