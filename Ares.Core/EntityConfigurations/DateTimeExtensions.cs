d3cd192018a3717db0be3e72308866ff39ebcff4
Babeckis <ababeckis@dcscorp.com>
2023-01-05 21:14:13 -0500

    Separated out datetime utc extensions from the entity configuration

---
+++ Ares.Core/EntityConfigurations/DateTimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Google.Protobuf.WellKnownTypes;
+
+namespace Ares.Core.EntityConfigurations;
+
+public static class DateTimeExtensions
+{
+  public static Timestamp ToTimestampUtc(this DateTime dateTime)
+  {
+    var utc = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
+    return utc.ToTimestamp();
+  }
+}
--- Ares.Core/EntityConfigurations/ExecutionInfoEntityConfiguration.cs
+++ Ares.Core/EntityConfigurations/ExecutionInfoEntityConfiguration.cs
@@ -1,5 +1,4 @@
 ﻿using Ares.Messaging;
-using Google.Protobuf.WellKnownTypes;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -12,15 +11,9 @@ internal class ExecutionInfoEntityConfiguration : AresEntityTypeBaseConfiguratio
     base.Configure(builder);
     builder.ToTable("ExecutionInfos");
     builder.Property(info => info.TimeStarted)
-      .HasConversion(timestamp => timestamp.ToDateTime(), time => ToTimestampUtc(time));
+      .HasConversion(timestamp => timestamp.ToDateTime(), time => time.ToTimestampUtc());
 
     builder.Property(info => info.TimeFinished)
-      .HasConversion(timestamp => timestamp.ToDateTime(), time => ToTimestampUtc(time));
-  }
-
-  private static Timestamp ToTimestampUtc(DateTime dateTime)
-  {
-    var utc = DateTime.SpecifyKind(dateTime, DateTimeKind.Utc);
-    return utc.ToTimestamp();
+      .HasConversion(timestamp => timestamp.ToDateTime(), time => time.ToTimestampUtc());
   }
 }
