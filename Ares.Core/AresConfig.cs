2731c431a290f5d62465efb18d85ecd92b94b74a
Nick Kleiner <nkleiner@dcscorp.com>
2025-01-30 10:36:33 -0500

    Added an AresConfig to give the core a place to store file paths to data like campaign templates, device information and campaign results. Updated various proto messages and implementations accordingly

---
+++ Ares.Core/AresConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace Ares.Core
+{
+  public static class AresConfig
+  {
+    public static string ResultsPath { get; set; } = string.Empty;
+
+    public static string TemplatePath { get; set; } = string.Empty;
+
+    public static string DevicesPath { get; set; } = string.Empty;
+  }
+}
