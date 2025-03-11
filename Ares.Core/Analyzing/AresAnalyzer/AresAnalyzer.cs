eb3d5a71e19c4cdc97f097584446d6e4e97cbd40
Nick Kleiner <nkleiner@dcscorp.com>
2025-03-11 15:21:35 -0400

    Moved analyzer serivce and base analyzer implementation into AresLib

---
+++ Ares.Core/Analyzing/AresAnalyzer/AresAnalyzer.cs
@@ -0,0 +1,38 @@
+﻿using Ares.Core.Analyzing;
+using Ares.Messaging;
+using Ares.Messaging.Analyzing;
+
+namespace AresAnalyzer;
+
+public class AresAnalyzer : AnalyzerBase<AnalysisInformation>
+{
+  readonly Uri _address;
+
+  public AresAnalyzer(string name, Uri fullAddress) : base(name, new Version(1, 0), fullAddress.OriginalString)
+  {
+    _address = fullAddress;
+  }
+
+  protected override async Task<Analysis> AnalyzeMessage(ExperimentResult result, AnalysisInformation input, CancellationToken cancellationToken)
+  {
+    var client = ClientStore.AresAnalyzingClient;
+    var response = await client.AnalyzerAsync(new AnalysisRequest { Placeholder = -1.0 });
+
+    return new Analysis
+    {
+      Analyzer = new() { Name = Name, Type = GetType().Name, Version = Version.ToString() },
+      Result = Convert.ToSingle(response.Value)
+    };
+  }
+
+  public void Init()
+  {
+    ClientStore.CreateClient(_address);
+  }
+
+  public override bool InputSupported(string fullTypeName)
+  {
+    //Given the limited nature of educational ARES, we'll assume we support whatever input students provide
+    return true;
+  }
+}
