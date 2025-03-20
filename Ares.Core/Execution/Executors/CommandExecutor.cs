144924e7bfa7b31a56a968a17eeb0b32a06fbcb6
Nick Kleiner <nkleiner@dcscorp.com>
2025-03-20 14:09:15 -0400

    Adding command descriptions and names to the command results object

--- Ares.Core/Execution/Executors/CommandExecutor.cs
+++ Ares.Core/Execution/Executors/CommandExecutor.cs
@@ -50,7 +50,7 @@ public class CommandExecutor : IExecutor<CommandResult, CommandExecutionStatus>
       Status.State = ExecutionState.Failed;
       _stateSubject.OnNext(Status);
       _stateSubject.OnCompleted();
-      return ExecutorResultHelpers.CreateCommandResult(Template.UniqueId, null, DateTime.UtcNow, DateTime.UtcNow);
+      return ExecutorResultHelpers.CreateCommandResult(Template, null, DateTime.UtcNow, DateTime.UtcNow);
     }
 
     var timeStarted = DateTime.UtcNow;
@@ -67,7 +67,7 @@ public class CommandExecutor : IExecutor<CommandResult, CommandExecutionStatus>
     _stateSubject.OnNext(Status);
     _stateSubject.OnCompleted();
 
-    return ExecutorResultHelpers.CreateCommandResult(Template.UniqueId, result, timeStarted, DateTime.UtcNow);
+    return ExecutorResultHelpers.CreateCommandResult(Template, result, timeStarted, timeStarted);
   }
 
   private async Task<DeviceCommandResult> InternalExecute(CancellationToken token)
